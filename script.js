d98149bc78ff9ae8b48fb5a154caefc7ed8c7043
aissgth <aissgithub@gmail.com>
2025-07-02 01:08:36 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleSub(id) {
-  const subMenu = document.getElementById(`sub-${id}`);
-  if (subMenu.classList.contains('show')) {
-    subMenu.classList.remove('show');
-  } else {
-    document.querySelectorAll('.sub-options').forEach(el => el.classList.remove('show'));
-    subMenu.classList.add('show');
-  }
-}
