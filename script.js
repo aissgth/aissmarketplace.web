883faf888b9ba9c47794206d9f2bb2d01440897e
aissgth <aissgithub@gmail.com>
2025-07-02 18:03:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function toggleSub(id) {
     subMenu.classList.add('show');
   }
 }
+
+function startQRISPayment() {
+  alert("Redirecting to QRIS payment...");
+  window.location.href = "payqris.html"; // Ganti dengan URL pembayaran QRIS yang sebenarnya
+}
+
+function startDanaPayment() {
+  alert("Redirecting to DANA payment...");
+  window.location.href = "paydana.html"; // Ganti dengan URL pembayaran DANA yang sebenarnya
+}
+
+function startGopayPayment() {
+  alert("Redirecting to GOPAY payment...");
+  window.location.href = "paygopay.html"; // Ganti dengan URL pembayaran GOPAY yang sebenarnya
+}
